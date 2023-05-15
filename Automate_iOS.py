3c587d280b8a510a4a207e428d7a4166f1e56c10
pragya-28 <39649873+pragya-28@users.noreply.github.com>
2023-05-15 23:14:22 +0530

    Update Automate_iOS.py

--- Automate_iOS.py
+++ Automate_iOS.py
@@ -4,42 +4,55 @@ from selenium import webdriver
 import time
 from selenium.webdriver.common.keys import Keys
 
-username = os.getenv("BROWSERSTACK_USERNAME")
-access_key = os.getenv("BROWSERSTACK_ACCESS_KEY")
-build_name = os.getenv("BROWSERSTACK_BUILD_NAME")
+userName = os.environ.get("BROWSERSTACK_USERNAME")
+accessKey = os.environ.get("BROWSERSTACK_ACCESS_KEY")
+buildName = os.environ.get("JENKINS_LABEL", "0")
 
-v1 = {
-"os_version" : "16",
-"device" : "iPhone 14",
-"browserstack.local" : "false",
-'name': 'BStack-[Jenkins] Sample Test',
-'build': build_name,
-'browserstack.user': username,
-'browserstack.key': access_key
+versions = [
+{
+    "osVersion" : "16",
+    "deviceName" : "iPhone 14",
+    "sessionName" : "BStack Build Name: " + buildName,
+    "seleniumVersion" : "4.0.0",
+    "userName": userName,
+    "accessKey": accessKey
+},
+{
+    "osVersion" : "10.0",
+    "deviceName" : "Samsung Galaxy S20",
+    "sessionName" : "BStack Build Name: " + buildName,
+    "seleniumVersion" : "4.0.0",
+    "userName": userName,
+    "accessKey": accessKey
 }
+]
 
 
-driver = webdriver.Remote(
-    command_executor='https://hub-cloud.browserstack.com/wd/hub',
-    desired_capabilities=v1)
-driver.maximize_window()
-driver.get("https://www.google.com/")
-driver.find_element("name", "q").send_keys("BrowserStack")
-time.sleep(3)
-try:
-    print('pressing enter on iphone14')
-    driver.find_element("name", "btnK").send_keys(Keys.ENTER)
+for i in versions:
+    print(i['os'])
+    options = ChromeOptions()
+    options.set_capability('bstack:options', i)
+    driver = webdriver.Remote(
+    command_executor="https://hub.browserstack.com/wd/hub",
+    options=options)
+    driver.maximize_window()
+    driver.get("https://www.google.com/")
+    driver.find_element("name", "q").send_keys("BrowserStack")
     time.sleep(3)
-    driver.execute_script(
-        'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed", "reason": "Sample test case for CHROME successful"}}'
-    )
-except Exception as err:
-    message = "Exception: " + "btnK was not found for CHROME"
-    driver.execute_script(
-        'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": '
-        + json.dumps(message)
-        + "}}"
-    )
-driver.close()
-driver.quit()
-print('test fini')
+    try:
+
+        driver.find_element("name", "btnK").send_keys(Keys.ENTER)
+        time.sleep(3)
+        driver.execute_script(
+            'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed", "reason": "Sample test case for CHROME successful"}}'
+        )
+    except Exception as err:
+        message = "Exception: " + "btnK was not found for CHROME"
+        driver.execute_script(
+            'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": '
+            + json.dumps(message)
+            + "}}"
+        )
+    driver.close()
+    driver.quit()
+    print('test fini')
