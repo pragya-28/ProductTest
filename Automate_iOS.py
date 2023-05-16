bf81e01b8a4c783cead9ea98951557198aca0c41
pragya-28 <39649873+pragya-28@users.noreply.github.com>
2023-05-16 11:36:49 +0530

    Update Automate_iOS.py

--- Automate_iOS.py
+++ Automate_iOS.py
@@ -1,31 +1,43 @@
-import appium
 import os
 from appium import webdriver
-from appium.webdriver.common.mobileby import MobileBy
-from appium.webdriver.common.touch_action import TouchAction
+from appium.options.android import UiAutomator2Options
+from appium.webdriver.common.appiumby import AppiumBy
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 user_name = os.getenv("BROWSERSTACK_USERNAME")
 access_key = os.getenv("BROWSERSTACK_ACCESS_KEY")
 build_name = os.environ.get("JENKINS_LABEL", "0")
-app = os.getenv("BROWSERSTACK_APP_ID")
 
-desired_cap = {
-    'app': 'com.android.chrome',
-    'deviceName': 'Samsung Galaxy S23',
-    'platformName' : 'android',
-    'platformVersion': '13.0',
-    'build': build_name,
-    'appActivity': 'com.google.android.apps.chrome.Main'
-}
+options = UiAutomator2Options().load_capabilities({
+    "platformName" : "android",
+    "platformVersion" : "9.0",
+    "deviceName" : "Google Pixel 3",
 
-driver = webdriver.Remote("https://"+user_name+":"+access_key+"@hub-cloud.browserstack.com/wd/hub", desired_cap)
+    "app" : "https://www.browserstack.com/app-automate/sample-apps/android/WikipediaSample.apk",
 
-wait = WebDriverWait(driver, 10)
-search_box = wait.until(EC.presence_of_element_located((MobileBy.ID, "com.android.chrome:id/search_box_text")))
-search_box.click()
-search_input = wait.until(EC.presence_of_element_located((MobileBy.ID, "com.android.chrome:id/url_bar")))
-search_input.send_keys("browserstack.com")
-driver.press_keycode(66)
+    'bstack:options' : {
+        "projectName" : "Project - Testing",
+        "buildName" : "testing-build-1",
+        "sessionName" : "first_test",
+
+        "userName" : user_name,
+        "accessKey" : access_key
+    }
+})
+
+driver = webdriver.Remote("http://hub.browserstack.com/wd/hub", options=options)
+search_element = WebDriverWait(driver, 30).until(
+    EC.element_to_be_clickable((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia"))
+)
+search_element.click()
+search_input = WebDriverWait(driver, 30).until(
+    EC.element_to_be_clickable(
+        (AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text"))
+)
+search_input.send_keys("BrowserStack")
+time.sleep(5)
+search_results = driver.find_elements(AppiumBy.CLASS_NAME, "android.widget.TextView")
+assert (len(search_results) > 0)
 driver.quit()
