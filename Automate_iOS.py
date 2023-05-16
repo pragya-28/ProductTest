51527a2df393354d477875a67ca24b74bad4ff4a
pragya-28 <39649873+pragya-28@users.noreply.github.com>
2023-05-16 13:08:59 +0530

    Update Automate_iOS.py

--- Automate_iOS.py
+++ Automate_iOS.py
@@ -1,1 +1,37 @@
+import os
+import json
+from selenium import webdriver
+import time
+from selenium.webdriver.common.keys import Keys
 
+userName = os.environ.get("BROWSERSTACK_USERNAME")
+accessKey = os.environ.get("BROWSERSTACK_ACCESS_KEY")
+buildName = os.environ.get("JENKINS_LABEL", "0")
+
+versions = [
+{
+    "osVersion" : "14",
+    "deviceName" : "iPhone 12",
+    "sessionName" : "BStack Build Name: " + buildName,
+    "seleniumVersion" : "4.0.0",
+    "userName": userName,
+    "accessKey": accessKey
+}
+]
+
+for i in versions:
+    print(i['osVersion'])
+    options = webdriver.ChromeOptions()
+    options.set_capability('bstack:options', i)
+    driver = webdriver.Remote(
+    command_executor="https://hub.browserstack.com/wd/hub",
+    options=options)
+    driver.maximize_window()
+    time.sleep(3)
+    driver.get("https://www.browserstack.com/")
+    time.sleep(3)
+    #driver.find_element("name", "q").send_keys("BrowserStack")
+   
+    driver.close()
+    driver.quit()
+    print('test finish')
