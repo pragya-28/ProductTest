3ecf1044b31722cee35ef2467b0ee79a04682313
pragya-28 <39649873+pragya-28@users.noreply.github.com>
2023-05-16 12:23:45 +0530

    Create Automate_iOS_Android.py

---
+++ Automate_iOS_Android.py
@@ -0,0 +1,25 @@
+from appium import webdriver
+
+userName = os.environ.get("BROWSERSTACK_USERNAME")
+accessKey = os.environ.get("BROWSERSTACK_ACCESS_KEY")
+buildName = os.environ.get("JENKINS_LABEL", "0")
+
+options = {
+    "platformName": "Android",
+    "osVersion" : "10.0",
+    "deviceName" : "Samsung Galaxy S20",
+    "browserName": "Chrome",
+    "sessionName" : "BStack Build Name: " + buildName,
+    "seleniumVersion" : "4.0.0",
+    "userName": userName,
+    "accessKey": accessKey
+}
+
+driver = webdriver.Remote(
+    command_executor="https://hub.browserstack.com/wd/hub",
+    options)
+driver.get('https://www.google.com')
+search_box = driver.find_element_by_name('q')
+search_box.send_keys('browserstack')
+search_box.submit()
+driver.quit()
