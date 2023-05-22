5ffa3fa056fd32a81f58eadccc39e4c43138bdea
Pragya Shukla <topragyashukla@gmail.com>
2023-05-22 16:22:20 +0530

    iOS device addition

--- Automate_iOS.py
+++ Automate_iOS.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.keys import Keys
 
 userName = os.environ.get("BROWSERSTACK_USERNAME")
 accessKey = os.environ.get("BROWSERSTACK_ACCESS_KEY")
-buildName = os.environ.get("JENKINS_LABEL", "0")
+buildName = os.environ.get("JENKINS_LABEL", "Sample Automate test on iOS")
 
 versions = [
 {
@@ -16,11 +16,35 @@ versions = [
     "seleniumVersion" : "4.0.0",
     "userName": userName,
     "accessKey": accessKey
+},
+{
+    "osVersion" : "16",
+    "deviceName" : "iPhone 14 Pro Max",
+    "sessionName" : "BStack Build Name: " + buildName,
+    "seleniumVersion" : "4.0.0",
+    "userName": userName,
+    "accessKey": accessKey
+},
+{
+    "osVersion" : "14",
+    "deviceName" : "iPad Air 4",
+    "sessionName" : "BStack Build Name: " + buildName,
+    "seleniumVersion" : "4.0.0",
+    "userName": userName,
+    "accessKey": accessKey
+},
+{
+    "osVersion" : "13",
+    "deviceName" : "iPad Pro 12.9 2020",
+    "sessionName" : "BStack Build Name: " + buildName,
+    "seleniumVersion" : "4.0.0",
+    "userName": userName,
+    "accessKey": accessKey
 }
 ]
 
 for i in versions:
-    print(i['osVersion'])
+    print(i['deviceName'])
     options = webdriver.ChromeOptions()
     time.sleep(3)
     options.set_capability('bstack:options', i)
